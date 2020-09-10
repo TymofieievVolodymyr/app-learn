e727995e25999ab30457bf75f3f0687b637dbe6b
Vovanium <vovanium.timofeev@gmail.com>
2020-09-11 00:52:53 +0300

    add BurgerBuilder and css module

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 
 import Layout from "./componentns/Layout/Layout";
 
+import BurgerBuilder from "./componentns/BurgerBuilder/BurgerBuilder";
+
 class App extends Component {
     render() {
         return (
             <div>
                 <Layout>
-                    <p>Test</p>
+                    <BurgerBuilder/>
                 </Layout>
             </div>
         );
--- src/componentns/Layout/Layout.js
+++ src/componentns/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Auxiliary from "../../hoc/Auxiliary";
+import classes from "./Layout.module.css"
 
 const Layout = (props) => (
     <Auxiliary>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxiliary>
