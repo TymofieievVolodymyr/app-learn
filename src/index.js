3fff8e9abc2580ee1792bda8a747b2c47a95d1b1
Vovanium <vovanium.timofeev@gmail.com>
2020-09-28 12:09:35 +0300

    added redux-thunk and middleware for async actions

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from "redux";
-import {Provider} from "react-redux";
 import burgerBuilder from "./store/reducers/burgerBuilder";
+import {createStore, applyMiddleware, compose} from "redux";
+import {Provider} from "react-redux";
+import thunk from 'redux-thunk';
 
-const store = createStore(burgerBuilder,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(burgerBuilder, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
