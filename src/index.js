b93d295b46678c72a9039b0a4807b18dc5a51bf0
Vovanium <vovanium.timofeev@gmail.com>
2020-09-28 11:11:14 +0300

    added basic Redux Dev extension

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {createStore} from "redux";
 import {Provider} from "react-redux";
 import reducer from "./store/reducer";
 
-const store = createStore(reducer)
+const store = createStore(reducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
 registerServiceWorker();
